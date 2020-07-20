d8a6a1ec113e68de932858b9f98f7f72ce9ba60d
shashank saxena <shashank.cse20@gmail.com>
2020-07-20 23:15:22 +0530

    Update postslist.js

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,1 +1,36 @@
+var card;
 
+function openpopup(id) { // Get the modal
+
+
+    var modal = document.getElementById("modaldelete");
+
+    modal.style.display = "block";
+
+    card = id;
+    console.log(card);
+}
+
+function removecard() {
+    console.log("in remove card method" +
+        card)
+    id = card;
+    document.getElementById("modaldelete").style.display = "none";
+    if (id == "first") {
+        document.getElementById("card1").style.display = "none";
+    } else if (id == "second") {
+        document.getElementById("card2").style.display = "none";
+    } else if (id == "third") {
+        document.getElementById("card3").style.display = "none";
+    } else if (id == "fourth") {
+        document.getElementById("card4").style.display = "none";
+    } else if (id == "fifth") {
+        document.getElementById("card5").style.display = "none";
+    }
+    return false;
+}
+
+function closemodal() {
+    var modal = document.getElementById("modaldelete");
+    modal.style.display = "none";
+}
